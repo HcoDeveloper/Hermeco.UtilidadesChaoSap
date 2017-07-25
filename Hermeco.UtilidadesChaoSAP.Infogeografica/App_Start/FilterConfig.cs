7c10a600be988658ec75f065211ce51c8baa6b70
jacksson.mosquera@offcorss.com <1@offcorss.com>
2017-07-25 14:08:54 -0500

    Carga Inicial

---
+++ Hermeco.UtilidadesChaoSAP.Infogeografica/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Hermeco.UtilidadesChaoSAP.Infogeografica
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Hermeco.UtilidadesChaoSAP.Infogeografica/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Hermeco.UtilidadesChaoSAP.Infogeografica.WebApiApplication" Language="C#" %>
